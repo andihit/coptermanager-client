8426ec1078a3f4f597908e7756710ed0ff4805be
Andreas Gerstmayr <andihit@gmail.com>
2014-08-10 20:52:28 +0200

    exclude nodejs modules from browser build

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,15 @@ var concat = require('gulp-concat');
 
 gulp.task('coffee', function() {
   gulp.src('./src/**/*.coffee')
-    .pipe(coffee({}).on('error', gutil.log))
+    .pipe(coffee().on('error', gutil.log))
     .pipe(gulp.dest('./lib/'))
 });
 
 gulp.task('browser', ['coffee'], function() {
   gulp.src(['./lib/index.js'])
-    .pipe(browserify())
-    .pipe(concat('browserlib.js'))
+    .pipe(browserify({
+      ignore: ['xmlhttprequest', './serialportclient']
+    }))
+    .pipe(concat('coptermanager-browser.js'))
     .pipe(gulp.dest('./dist'));
 });
